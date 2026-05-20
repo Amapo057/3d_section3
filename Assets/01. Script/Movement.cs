a38e5737bc2299d17fee951c7665355b833d0e72
김지성 <jiseong2002@naver.com>
2026-05-20 16:46:51 +0900

    Rotation Input

--- Assets/01. Script/Movement.cs
+++ Assets/01. Script/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength = 1000f;
     Rigidbody rb;
 
@@ -12,6 +13,7 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
 
@@ -23,11 +25,26 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
         if (thrust.IsPressed())
         {
             // 로컬 방향으로 힘을 가함
             // vector3.up은 실질적으론 0, 1, 0
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput != 0)
+        {
+            Debug.Log(rotationInput);
         }
     }
 }
