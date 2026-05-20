a42d2da99adb102985047d5855081106ee5ba01b
김지성 <jiseong2002@naver.com>
2026-05-20 15:42:29 +0900

    Using AddRelativeForce

--- Assets/01. Script/Movement.cs
+++ Assets/01. Script/Movement.cs
@@ -5,6 +5,9 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] float thrustStrength = 1000f;
+    Rigidbody rb;
+
 
     private void OnEnable()
     {
@@ -14,15 +17,17 @@ public class Movement : MonoBehaviour
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("You Input Spacebar");
+            // 로컬 방향으로 힘을 가함
+            // vector3.up은 실질적으론 0, 1, 0
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
     }
 }
