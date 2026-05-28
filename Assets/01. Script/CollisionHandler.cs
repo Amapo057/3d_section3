d4da3a1ad70d15af7243367cc4211aed61b2764a
김지성 <jiseong2002@naver.com>
2026-05-28 16:09:13 +0900

    Respawn Using SceneManager

--- Assets/01. Script/CollisionHandler.cs
+++ Assets/01. Script/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -16,7 +17,13 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("end");
                 break;
             default:
+                ReloadLevel();
                 break;
+        }
+        void ReloadLevel()
+        {
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentScene);
         }
     }
 }
